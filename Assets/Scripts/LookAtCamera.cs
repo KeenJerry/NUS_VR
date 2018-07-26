28da82181ab300541440498a673fab68b5ef8d6e
Zhou Kan <t0916124@nus.edu.sg>
2018-07-26 13:04:08 +0800

    add collider to the environment

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -7,7 +7,7 @@ public class LookAtCamera : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        camera = GameObject.Find("VRCamera");
+        camera = GameObject.Find("Camera");
     }
 	
 	// Update is called once per frame
