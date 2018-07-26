a60a736c0cc151173a073629deb396f2d3efa404
Wu Jiang <wujiang5521@qq.com>
2018-07-26 22:59:08 +0800

    refector menu

---
+++ Assets/Scripts/FadeMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeMenu : MonoBehaviour {
+    public SpriteRenderer[] imgs;
+    public TextMesh[] texts;
+    public MeshRenderer[] objs;
+
+    public float minDis = 15;
+    public float maxDis = 20;
+
+    private Transform camera;
+    private float lastDis;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.Find("VRCamera").transform;
+
+        lastDis = (minDis + maxDis) / 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float dis = (transform.position - camera.transform.position).magnitude;
+		if (lastDis >= minDis && lastDis <= maxDis)
+        {
+            float opacity;
+            if (dis <= minDis) opacity = 1;
+            else if (dis >= maxDis) opacity = 0;
+            else opacity = (maxDis - dis) / (maxDis - minDis);
+            
+            foreach(SpriteRenderer img in imgs)
+            {
+                Color color = img.color;
+                color.a = opacity;
+                img.color = color;
+            }
+            foreach(TextMesh text in texts)
+            {
+                Color color = text.color;
+                color.a = opacity;
+                text.color = color;
+            }
+            foreach(MeshRenderer obj in objs)
+            {
+                Color color = obj.material.color;
+                color.a = opacity;
+                obj.material.color = color;
+            }
+        }
+        lastDis = dis;
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public Transform location;
+    public float speed = 20;
+    public float maxDis = 20;
+    public float minDis = 15;
+    private Transform camera;
+    private bool open = false;
+    private Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+        camera = GameObject.Find("VRCamera").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(open);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (open)
+            {
+                open = false;
+            }
+            else
+            {
+                open = true;
+                dir = (location.position - camera.position).normalized;
+                transform.position = dir * maxDis + location.position;
+            }
+        }
+            
+
+        float dis = (transform.position - camera.position).magnitude;
+        if (open)
+        {
+            if (dis > minDis)
+                transform.position -= dir * speed * Time.deltaTime;
+        }
+        else
+        {
+            if (dis < maxDis)
+                transform.position += dir * speed * Time.deltaTime;
+        }
+	}
+}
