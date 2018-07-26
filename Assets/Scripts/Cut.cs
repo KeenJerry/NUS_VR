abdc81ee76c101ad4570f546250c69f50c3079e9
Zhou Kan <t0916124@nus.edu.sg>
2018-07-26 16:27:18 +0800

    cut food feature complete

---
+++ Assets/Scripts/Cut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject Trajectory;
+	void Start () {
+        Trajectory = GameObject.Find("Trajectory");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("food"))
+        {   
+            if(Trajectory)
+                Trajectory.GetComponent<LaunchFood>().cutFood(other.gameObject);
+        }
+    }
+}
