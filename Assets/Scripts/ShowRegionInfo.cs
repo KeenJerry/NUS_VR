ef1553cf27d2f0585d9101c8f23e6170f9feb367
Zhou Kan <t0916124@nus.edu.sg>
2018-07-27 21:30:35 +0800

    ShowRegionInfo

--- Assets/Scripts/ShowRegionInfo.cs
+++ Assets/Scripts/ShowRegionInfo.cs
@@ -1,29 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 public class ShowRegionInfo : MonoBehaviour {
 
     // Use this for initialization
-    GameObject Info;
+    public List<GameObject> Info;
+    
 
     public void ShowInfo()
     {
+        
         for(int i = 0; i < Player.instance.handCount; i++)
         {
             Hand hand = Player.instance.GetHand(i);
 
             if(hand.controller != null)
             {
-                if (hand.controller.GetTouchDown(Valve.VR.EVRButtonId.k_EButton_DPad_Down))
+                if (hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
                 {
-                    Info.SetActive(true);
+                   foreach(GameObject info in Info)
+                    {
+                        info.SetActive(true);
+                    }
+                    return;
+                }
+                if (hand.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+                {
+                    foreach (GameObject info in Info)
+                    {
+                        info.SetActive(false);
+                    }
                     return;
                 }
             }
         }
-        Info.SetActive(false);
     }
 
 	void Start () {
