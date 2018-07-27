9e90f09a358afa5f6612a8a5a0179256f547df71
Wu Jiang <wujiang5521@qq.com>
2018-07-27 14:21:58 +0800

    menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class PauseMenu : MonoBehaviour {
     public Transform location;
@@ -11,15 +12,17 @@ public class PauseMenu : MonoBehaviour {
     private bool open = false;
     private Vector3 dir;
 
+    public Transform cursor;
+    public TextMesh volumnValue;
+
 	// Use this for initialization
 	void Start () {
         camera = GameObject.Find("VRCamera").transform;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(open);
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (applicationDown())
         {
             if (open)
             {
@@ -32,18 +35,75 @@ public class PauseMenu : MonoBehaviour {
                 transform.position = dir * maxDis + location.position;
             }
         }
-            
 
         float dis = (transform.position - camera.position).magnitude;
         if (open)
         {
             if (dis > minDis)
                 transform.position -= dir * speed * Time.deltaTime;
+
+            Vector2 pos = touchValue();
+            if (pos.magnitude > 0.5f)
+            {
+                float degree;
+                if (pos.x == 0) {
+                    if (pos.y > 0) degree = 90;
+                    else degree = 270;
+                } else {
+                    degree = (float)(System.Math.Atan(pos.y / pos.x) / System.Math.PI * 180);
+                    if (degree < 0) degree += 180;
+                    if (pos.y < 0) degree += 180;
+                }
+                cursor.localEulerAngles = new Vector3(0, 0, degree - 90);
+
+                // set value
+                int volumn = (int)degree / 3;
+                if (volumn < 20) volumn = 20 - volumn;
+                else if (volumn <= 30) volumn = 0;
+                else if (volumn <= 40) volumn = 100;
+                else volumn = 140 - volumn;
+                volumnValue.text = "" + volumn;
+            }
         }
         else
         {
             if (dis < maxDis)
                 transform.position += dir * speed * Time.deltaTime;
+            else
+                transform.position = new Vector3(0, maxDis, 0);
         }
 	}
+
+    bool applicationDown()
+    {
+        for (int i = 0; i < Player.instance.handCount; i++)
+        {
+            Hand hand = Player.instance.GetHand(i);
+            if (hand.controller != null)
+                if (hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_ApplicationMenu))
+                    return true;
+        }
+        return false;
+    }
+
+    Vector2 touchValue()
+    {
+        for (int i = 0; i < Player.instance.handCount; i++)
+        {
+            Hand hand = Player.instance.GetHand(i);
+            if (hand.controller != null)
+                if (hand.controller.GetTouchDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+                {
+                    Vector2 pos = hand.controller.GetAxis();
+                    if (pos != Vector2.zero)
+                        return pos;
+                }
+        }
+        return Vector2.zero;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
 }
