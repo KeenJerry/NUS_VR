931f4a1f943f5639dfa2bbf0def52b41db0730df
Wu Jiang <wujiang5521@qq.com>
2018-07-27 14:38:13 +0800

    ButtonIcon

--- Assets/Scripts/ButtonIcon.cs
+++ Assets/Scripts/ButtonIcon.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class ButtonIcon : MonoBehaviour {
     private void OnTriggerEnter(Collider other)
     {
-        // if (other.gameObject.GetComponent<Cut>() != null)
-           // transform.parent.gameObject.GetComponent<Button>().trigger();
+        if (other.gameObject.GetComponent<Cut>() != null)
+            transform.parent.gameObject.GetComponent<FruitButton>().trigger();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // if (other.gameObject.GetComponent<Cut>() != null)
-           // transform.parent.gameObject.GetComponent<Button>().trigger();
+        if (collision.gameObject.GetComponent<Cut>() != null)
+            transform.parent.gameObject.GetComponent<FruitButton>().trigger();
     }
 }
