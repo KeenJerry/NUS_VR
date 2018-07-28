4939e7baf663e1632c7925f2f89cc54c328647aa
Wu Jiang <wujiang5521@qq.com>
2018-07-28 19:29:18 +0800

    hint color

--- Assets/Scripts/RiseHintController.cs
+++ Assets/Scripts/RiseHintController.cs
@@ -18,20 +18,29 @@ public class RiseHintController : MonoBehaviour {
     public Color good = new Color(0, 0, 1);
 
 
+    private void Start()
+    {
+        hintsInitPos = new float[hints.Length];
+    }
+
     // Update is called once per frame
     void Update () {
-        hintsInitPos = new float[hints.Length];
 		for (int i = 0; i < hints.Length; i++)
             if (hints[i].gameObject.activeSelf)
             {
                 rise(hints[i], Time.deltaTime);
-                if (hints[i].gameObject.transform.position.y - hintsInitPos[i] > riseLength) hints[i].gameObject.SetActive(false);
+                if (hints[i].gameObject.transform.position.y - hintsInitPos[i] > riseLength)
+                {
+                    Debug.Log(hints[i].gameObject.transform.position.y + " " + hintsInitPos[i] + " " + riseLength);
+                    hints[i].gameObject.SetActive(false);
+                }
             }
                 
 	}
 
     public void hint(string content, HintType type, Vector3 pos)
     {
+        Debug.Log(content);
         Color color = new Color(1, 1, 1, 1);
         switch(type)
         {
@@ -51,11 +60,14 @@ public class RiseHintController : MonoBehaviour {
             if (!hints[i].gameObject.activeSelf)
                 break;
 
-        hintsInitPos[i] = pos.y + 1;
-        hints[i].gameObject.transform.position = new Vector3(pos.x, pos.y+1, pos.z);
-        hints[i].text = content;
-        hints[i].color = color;
-        hints[i].gameObject.SetActive(true);
+        if (i != hints.Length)
+        {
+            hintsInitPos[i] = pos.y + 1;
+            hints[i].gameObject.transform.position = new Vector3(pos.x, pos.y + 1, pos.z);
+            hints[i].text = content;
+            hints[i].color = color;
+            hints[i].gameObject.SetActive(true);
+        }
     }
 
     private void rise(TextMesh obj, float time)
