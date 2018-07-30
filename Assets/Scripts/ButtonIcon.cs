903a094dcbb0d8dd65fbfd0ff6fa0a373152164a
Zhou Kan <t0916124@nus.edu.sg>
2018-07-30 16:56:58 +0800

    reduce collider

--- Assets/Scripts/ButtonIcon.cs
+++ Assets/Scripts/ButtonIcon.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonIcon : MonoBehaviour {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<Cut>() != null)
-            transform.parent.gameObject.GetComponent<FruitButton>().trigger();
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("Trigger");
+    //    if (other.gameObject.GetComponent<Cut>() != null)
+    //        transform.parent.gameObject.GetComponent<FruitButton>().trigger();
+    //}
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collision");
         if (collision.gameObject.GetComponent<Cut>() != null)
             transform.parent.gameObject.GetComponent<FruitButton>().trigger();
     }
